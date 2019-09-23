5aead0ebb83bff16097753d074f61c2eeba607ed
2565497683zxc <L18810857853@163.com>
2019-09-23 16:36:45 +0800

    'ln'

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# client
-今天又是一个美好的一天
+# clients
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn run serve
+```
+
+### Compiles and minifies for production
+```
+yarn run build
+```
+
+### Run your tests
+```
+yarn run test
+```
+
+### Lints and fixes files
+```
+yarn run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="box">
+    <div class="main">
+
+    </div>
+    <div class="bottom">
+      <ul>
+        <li>
+          <i class="iconfont">&#xe637;</i>
+          <span>首页</span>
+        </li>
+        <li>
+          <i class="iconfont">&#xe637;</i>  
+          专题
+        </li>
+        <li>分类</li>
+        <li>购物车</li>
+        <li>我的</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+  .box{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+  }
+  .box .main{
+    width: 100%;
+    flex: 1;
+  }
+  .bottom{
+    width: 100%;
+    height: 50px;
+    /* bottom: 0; */
+    background: white;
+    box-shadow: 0 -3px 10px 0 rgba(0,0,0,.2);
+    /* z-index: 100; */
+  }
+  .bottom ul{
+    width: 100%;
+    height:100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+  }
+</style>
