a352f97ffe757bff7042243e6b6d31eb7e47ada8
2565497683zxc <L18810857853@163.com>
2019-09-24 14:09:04 +0800

    'ls'

--- src/components/homePage.vue
+++ src/components/homePage.vue
@@ -135,6 +135,128 @@
         <div class="home_hotgats_top">
             人气推荐
         </div>
+        <div class="home_hotgats_con">
+            <a href="#">
+                <img src="../images/12.png" alt="">
+                <div class="home_hotgats_con_acon">
+                    <div class="home_hotgats_con_acon_t">
+                        双攻蚕丝被 空调被
+                    </div>
+                    <div class="home_hotgats_con_acon_b">
+                        双攻蚕丝被 空调被
+                    </div>
+                    <div class="home_hotgats_con_acon_many">
+                        "￥"699
+                    </div>
+                </div>
+            </a>
+            <a href="#">
+                <img src="../images/13.png" alt="">
+                <div class="home_hotgats_con_acon">
+                    <div class="home_hotgats_con_acon_t">
+                        双攻蚕丝被 空调被
+                    </div>
+                    <div class="home_hotgats_con_acon_b">
+                        双攻蚕丝被 空调被
+                    </div>
+                    <div class="home_hotgats_con_acon_many">
+                        "￥"699
+                    </div>
+                </div>
+            </a>
+            <a href="#">
+                <img src="../images/14.png" alt="">
+                <div class="home_hotgats_con_acon">
+                    <div class="home_hotgats_con_acon_t">
+                        双攻蚕丝被 空调被
+                    </div>
+                    <div class="home_hotgats_con_acon_b">
+                        双攻蚕丝被 空调被
+                    </div>
+                    <div class="home_hotgats_con_acon_many">
+                        "￥"699
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="home_topGoodsBox">
+        <div class="home_topGoodsBox_topGood">
+            专题精选
+        </div>
+        <div class="home_topGoodsBox_con">
+            <div class="home_topGoodsBox_con_c">
+                <div class="home_topGoodsBox_con_con">
+                    <ul>
+                        <li class="li1">
+                            <a href="#">
+                                <img src="../images/a1.jpg" alt="">
+                                <div class="home_a_con">
+                                    撒到我萨达的娃啊低洼
+                                    <span>￥0元起</span>
+                                </div>
+                                <div class="home_a_con_topGood">
+                                    专业运动品牌同矿，毛毛虫鞋买二送一
+                                </div>
+                            </a>
+                        </li>
+                        <li class="li2">
+                            <a href="#">
+                                <img src="../images/a2.jpg" alt="">
+                                <div class="home_a_con">
+                                    撒到我萨达的娃啊低洼
+                                    <span>￥0元起</span>
+                                </div>
+                                <div class="home_a_con_topGood">
+                                    专业运动品牌同矿，毛毛虫鞋买二送一
+                                </div>
+                            </a>
+                        </li>
+                        <li class="li3">
+                            <a href="#">
+                                <img src="../images/a3.jpg" alt="">
+                                <div class="home_a_con">
+                                    撒到我萨达的娃啊低洼
+                                    <span>￥0元起</span>
+                                </div>
+                                <div class="home_a_con_topGood">
+                                    专业运动品牌同矿，毛毛虫鞋买二送一
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="home_create">
+        <div class="home_create_top">
+            家居
+        </div>
+        <div class="home_create_con">
+            <a href="#">
+                <div class="home_create_con_img">
+                    <img src="../images/b1.png" alt="">
+                </div>
+                <div class="home_create_con_name">
+                    撒法发是娃娃福娃
+                </div>
+                <div class="home_create_con_Price">
+                    ￥599
+                </div>
+            </a>
+            <a href="#">
+                <div class="home_create_con_img">
+                    <img src="../images/b1.png" alt="">
+                </div>
+                <div class="home_create_con_name">
+                    撒法发是娃娃福娃
+                </div>
+                <div class="home_create_con_Price">
+                    ￥599
+                </div>
+            </a>
+        </div>
     </div>
     </div>
 </template>
@@ -170,6 +292,10 @@ export default {
 }
 </script>
 <style scoped>
+    a{
+        text-decoration:none;
+        color:#000;
+    }
     .home{
         width: 100%;
         height: 100%;
@@ -257,7 +383,7 @@ export default {
     }
     .home_newGood{
         width: 100%;
-        height: 1rem;
+        height:500px;
         margin-top: .2rem;
         background: white;
     }
@@ -278,8 +404,6 @@ export default {
     .home_newGood .home_newGood_con a{
         width: 50%;
         height: 4rem;
-        text-decoration:none;
-        color:#000;
     }
     .home_newGood .home_newGood_con a img{
         opacity: 1;
@@ -302,5 +426,248 @@ export default {
         justify-content: center;
         align-items: center;
         font-size: .3rem;
+    }
+    .home_hotgats{
+        width: 100%;
+        height: 500px;
+        margin-top: .2rem;
+        background: white;
+    }
+    .home_hotgats .home_hotgats_top{
+        width: 100%;
+        height: 1rem;
+        background: white;
+        text-align: center;
+        font-size: .3rem;
+        line-height: 1rem;
+        letter-spacing: .02rem;
+        font-weight: bold;
+    }
+    .home_hotgats .home_hotgats_con{
+        width: 100%;
+        height: 450px;
+        padding: 0 .2rem;
+        min-height: 200px;
+    }
+    .home_hotgats .home_hotgats_con a{
+        height: 3rem;
+        position: relative;
+        padding: 0.1rem 0;
+        display: flex;
+    }
+    .home_hotgats .home_hotgats_con a::after{
+        content: '';
+        width: 200%;
+        height: 1px;
+        background: darkgrey;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        -webkit-transform-origin: 0;
+        transform-origin: 0;
+        -webkit-transform: scale(0.5);
+        transform: scale(0.5);
+    }
+    .home_hotgats .home_hotgats_con a img{
+        width: auto;
+        height: 100%;
+        opacity: 1;
+        -webkit-transition: all 1s;
+        transition: all 1s;
+    }
+    .home_hotgats .home_hotgats_con a .home_hotgats_con_acon{
+        flex: 1 1;
+        padding: 0.5rem 0;
+    }
+    .home_hotgats .home_hotgats_con a .home_hotgats_con_acon .home_hotgats_con_acon_t{
+        font-size: .32rem;
+    }
+    .home_hotgats .home_hotgats_con a .home_hotgats_con_acon .home_hotgats_con_acon_b{
+        color: darkgray;
+        font-size: .28rem;
+    }
+    .home_hotgats .home_hotgats_con a .home_hotgats_con_acon .home_hotgats_con_acon_many{
+        color: darkred;
+        font-size: .3rem;
+    }
+    .home_topGoodsBox{
+        width: 100%;
+        height: 312px;
+        margin-top: .2rem;
+        background: white;
+    }
+    .home_topGoodsBox .home_topGoodsBox_topGood{
+        height: 1rem;
+        text-align: center;
+        font-size: .3rem;
+        line-height: 1rem;
+        letter-spacing: .02rem;
+        font-weight: bold;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con{
+        height: auto;
+        padding-bottom: .2rem;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_c{
+        position: relative;
+        display: block;
+        width: 100%;
+        height: auto;
+        box-sizing: border-box;
+        visibility: visible;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_con{
+        position: relative;
+        display: block;
+        overflow: hidden;
+        height: auto;
+        margin: 0px;
+        padding: 0px;
+        transform: translate3d(0px, 0px, 0px);
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: row;
+        overflow: auto;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_con ul{
+        transform: translate3d(-310.625px, 0px, 0px);
+        
+        position: relative;
+        display: block;
+        margin: 0px -5px;
+        padding: 0px;
+        height: 252px;
+        width: 986.25px;
+        cursor: inherit;
+        box-sizing: border-box;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_con ul .li1{
+        position: absolute;
+        left: 657.5px;
+        top: 0px;
+        display: inline-block;
+        list-style-type: none;
+        vertical-align: top;
+        width: 318.75px;
+        height: auto;
+        box-sizing: border-box;
+        margin: auto 5px;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_con ul li a{
+        height: 5rem;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_con ul li a img{
+        opacity: 1;
+        -webkit-transition: all 1s;
+        transition: all 1s;
+        height: 4rem;
+        width: 100%;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_con ul li a .home_a_con{
+        font-size: .32rem;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_con ul li a .home_a_con span{
+        color: darkred;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_con ul li a .home_a_con_topGood{
+        font-size: .24rem;
+        color: gray;
+    }
+
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_con ul .li2{
+        position: absolute;
+        left: 328.75px;
+        top: 0px;
+        display: inline-block;
+        list-style-type: none;
+        vertical-align: top;
+        width: 318.75px;
+        height: auto;
+        box-sizing: border-box;
+        margin: auto 5px;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_con ul .li3{
+        position: absolute;
+        left: 986.25px;
+        top: 0px;
+        display: inline-block;
+        list-style-type: none;
+        vertical-align: top;
+        width: 318.75px;
+        height: auto;
+        box-sizing: border-box;
+        margin: auto 5px;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_con ul li a{
+        height: 5rem;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_con ul li a img{
+        opacity: 1;
+        -webkit-transition: all 1s;
+        transition: all 1s;
+        height: 4rem;
+        width: 100%;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_con ul li a .home_a_con{
+        font-size: .32rem;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_con ul li a .home_a_con span{
+        color: darkred;
+    }
+    .home_topGoodsBox .home_topGoodsBox_con .home_topGoodsBox_con_con ul li a .home_a_con_topGood{
+        font-size: .24rem;
+        color: gray;
+    }
+    .home_create{
+        width: 100%;
+        height: auto;
+    }
+    .home_create .home_create_top{
+        width: 100%;
+        height: 1rem;
+        text-align: center;
+        line-height: 1rem;
+        font-size: .3rem;
+    }
+    .home_create .home_create_con{
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .home_create .home_create_con a{
+        width: 50%;
+        height: auto;
+        background: white;
+        padding: .2rem;
+        position: relative;
+    }
+    .home_create .home_create_con a .home_create_con_img{
+        width: 100%;
+        min-height: 2rem;
+        height: auto;
+    }
+    .home_create .home_create_con a .home_create_con_img img{
+        opacity: 1;
+        -webkit-transition: all 1s;
+        transition: all 1s;
+        width: 100%;
+        height: auto;
+    }
+    .home_create .home_create_con a .home_create_con_name{
+        text-align: center;
+        font-size: .28rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        padding-bottom: .3rem;
+        padding-top: .1rem;
+    }
+    .home_create .home_create_con a .home_create_con_Price{
+        text-align: center;
+        font-size: .28rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        color: red;
     }
 </style>
--- src/components/my.vue
+++ src/components/my.vue
@@ -1,6 +1,67 @@
 <template>
-    <div>
-        my
+    <div class="my">
+        <div class="my_top">
+            <div class="my_top_left">
+
+            </div>
+            <div class="my_top_right">
+                <div>15323807318</div>
+                <div>普通用户</div>
+            </div>
+        </div>
+        <div class="my_con">
+            <div class="my_con_c" id="cons">
+                <i class="iconfont">&#xe6d0;</i>
+                <div>我的收藏</div>
+            </div>
+            <div class="my_con_c" id="cons">
+                <i class="iconfont">&#xe61d;</i>
+                <div>地址管理</div>
+            </div>
+            <div class="my_con_c">
+                <i class="iconfont">&#xe649;</i>
+                <div>我的订单</div>
+            </div>
+            <div class="my_con_c">
+                <i class="iconfont">&#xe70e;</i>
+                <div>周末拼单</div>
+            </div>
+            <div class="my_con_c">
+                <i class="iconfont">&#xe681;</i>
+                <div>优惠卷</div>
+            </div>
+            <div class="my_con_c">
+                <i class="iconfont">&#xe654;</i>
+                <div>优选购</div>
+            </div>
+            <div class="my_con_c">
+                <i class="iconfont">&#xe603;</i>
+                <div>我的红包</div>
+            </div>
+            <div class="my_con_c">
+                <i class="iconfont">&#xe609;</i>
+                <div>会员plus</div>
+            </div>
+            <div class="my_con_c">
+                <i class="iconfont">&#xe606;</i>
+                <div>邀请返利</div>
+            </div>
+            <div class="my_con_c">
+                <i class="iconfont">&#xe601;</i>
+                <div>意见反馈</div>
+            </div>
+            <div class="my_con_c">
+                <i class="iconfont">&#xe61e;</i>
+                <div>客服咨询</div>
+            </div>
+            <div class="my_con_c">
+                <i class="iconfont">&#xe6fa;</i>
+                <div>账户安全</div>
+            </div>
+        </div>
+        <div class="my_bottom">
+            退出登陆
+        </div>
     </div>
 </template>
 <script>
@@ -8,6 +69,75 @@ export default {
     
 }
 </script>
-<style lang="stylus" scoped>
-
+<style scoped>
+    .my{
+        width: 100%;
+        height: 100%;
+    }
+    .my .my_top{
+        height: 3rem;
+        background: url(../images/my1.png) no-repeat;
+        background-size: 100%;
+        padding: .7rem .4rem;
+        display: flex;
+            
+    }
+    .my .my_top .my_top_left{
+        width: 1.6rem;
+        background: url(../images/my2.png) no-repeat;
+        background-size: 100%;
+        height: 100%;
+        border-radius: 50%;
+    }
+    .my .my_top .my_top_right{
+        flex: 1 1;
+        padding: .2rem;
+        padding-right: 0;
+        height: 100%;
+    }
+    .my .my_top .my_top_right div{
+        height: 50%;
+        display: flex;
+        align-items: center;
+        color: white;
+        font-size: .3rem;
+    }
+    .my .my_con{
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .my .my_con .my_con_c{
+        width: 33.333%;
+        height: 2.5rem;
+        background: white;
+        border: .5px solid gainsboro;
+    }
+    .my .my_con #cons{
+        color: rgb(33, 150, 243);
+    }
+    .my .my_con #cons i{
+        color: rgb(33, 150, 243);
+    }
+    .my .my_con .my_con_c i{
+        display: block;
+        padding-top: .8rem;
+        padding-bottom: .2rem;
+        text-align: center;
+        font-size: .6rem;
+        color: #9e9e9e;
+    }
+    .my .my_con .my_con_c div{
+        text-align: center;
+        font-size: .3rem;
+    }
+    .my .my_bottom{
+        height: 1rem;
+        width: 100%;
+        margin: .2rem 0;
+        background: white;
+        text-align: center;
+        line-height: 1rem;
+        font-size: .3rem;
+    }
 </style>
